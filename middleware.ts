4dadab3df0955027b2dbd660f5434030d0f1ad57
半代大和 <y.handai1272@gmail.com>
2024-10-28 22:30:00 +0900

    chore(middleware): Add authentication middleware for the /projects directory

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+export const config = {
+  matcher: ['/projects/:path*'],
+}
+
+export function middleware(req: NextRequest) {
+  if (process.env.NODE_ENV === 'development') {
+    return NextResponse.next()
+  }
+
+  if (!process.env.BASIC_AUTH_PASSWORD) {
+    return NextResponse.next()
+  }
+
+  const basicAuth = req.headers.get('authorization')
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1] ?? ''
+    const [user, pwd] = atob(authValue).split(':')
+
+    if (user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASSWORD) {
+      return NextResponse.next()
+    }
+  }
+
+  return new Response('401 Unauthorized', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
