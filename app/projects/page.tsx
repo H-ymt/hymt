af51b13072d7a79bfe2122c095a1d3ee8d6f70e2
半代大和 <y.handai1272@gmail.com>
2024-11-24 16:07:11 +0900

    feat(button): Deleted secret project link

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,5 @@
 import Container from '@/app/components/container'
 import ProjectList from '@/app/components/project-list'
-import Button from '@/app/components/ui/button'
 import { getProjectsList } from '@/lib/microcms'
 
 import styles from './page.module.css'
@@ -15,12 +14,6 @@ export default async function ProjectsPage() {
       <h1>Projects</h1>
       <div className={styles.projects}>
         <ProjectList projects={data.contents} />
-      </div>
-
-      <div className={styles.button}>
-        <Button href="/" size="large" visual="secondary">
-          Secret Projects
-        </Button>
       </div>
     </Container>
   )
