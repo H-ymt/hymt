32eccf53ad2f2efedb2c84eaa767db98046a4674
半代大和 <y.handai1272@gmail.com>
2024-12-02 21:34:58 +0900

    feat(mode-toggle): Implement mode toggle button

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,15 +1,19 @@
 import GlobalMenu from '@/app/components/global-menu'
+import ThemeToggle from '@/app/components/theme-toggle'
 
 import styles from './index.module.css'
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.headerInner}>
-        <div className={styles.menuWrapper}>
-          <GlobalMenu />
+    <>
+      <header className={styles.header}>
+        <div className={styles.headerInner}>
+          <div className={styles.menuWrapper}>
+            <GlobalMenu />
+            <ThemeToggle />
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   )
 }
--- app/components/theme-toggle/index.tsx
+++ app/components/theme-toggle/index.tsx
@@ -1,7 +1,56 @@
+'use client'
+
+import { useTheme } from 'next-themes'
+
+import { MonitorIcon, MoonIcon, SunIcon } from '@/app/components/icons'
+
+import styles from './index.module.css'
+
 export default function ThemeToggle() {
+  const { setTheme, theme } = useTheme()
+
   return (
-    <div>
-      <button type="button">button</button>
-    </div>
+    <fieldset className={styles.themeToggle}>
+      <legend className="sr-only">Toggle Theme</legend>
+
+      <label className={styles.themeOption}>
+        <input
+          type="radio"
+          name="theme"
+          value="system"
+          checked={theme === 'system'}
+          onChange={() => setTheme('system')}
+          aria-label="System theme"
+          className="sr-only"
+        />
+        <MonitorIcon className={styles.icon} />
+      </label>
+
+      <label className={styles.themeOption}>
+        <input
+          type="radio"
+          name="theme"
+          value="light"
+          checked={theme === 'light'}
+          onChange={() => setTheme('light')}
+          aria-label="Light theme"
+          className="sr-only"
+        />
+        <SunIcon className={styles.icon} />
+      </label>
+
+      <label className={styles.themeOption}>
+        <input
+          type="radio"
+          name="theme"
+          value="dark"
+          checked={theme === 'dark'}
+          onChange={() => setTheme('dark')}
+          aria-label="Dark theme"
+          className="sr-only"
+        />
+        <MoonIcon className={styles.icon} />
+      </label>
+    </fieldset>
   )
 }
