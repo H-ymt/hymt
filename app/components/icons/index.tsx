3e5c16645bf27fdca6de14571e62e8e9883c81dc
半代大和 <y.handai1272@gmail.com>
2025-02-15 23:41:21 +0900

    feat(icons): Add accessibility attributes and titles to SVG icons

--- app/components/icons/index.tsx
+++ app/components/icons/index.tsx
@@ -2,18 +2,20 @@ import type { SVGProps } from 'react'
 
 export function GithubIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32" {...props}>
+    <svg role="img" width="1em" height="1em" viewBox="0 0 32 32" {...props}>
+      <title>GitHubアイコン</title>
       <path
         fill="currentColor"
         d="M16 .396c-8.839 0-16 7.167-16 16c0 7.073 4.584 13.068 10.937 15.183c.803.151 1.093-.344 1.093-.772c0-.38-.009-1.385-.015-2.719c-4.453.964-5.391-2.151-5.391-2.151c-.729-1.844-1.781-2.339-1.781-2.339c-1.448-.989.115-.968.115-.968c1.604.109 2.448 1.645 2.448 1.645c1.427 2.448 3.744 1.74 4.661 1.328c.14-1.031.557-1.74 1.011-2.135c-3.552-.401-7.287-1.776-7.287-7.907c0-1.751.62-3.177 1.645-4.297c-.177-.401-.719-2.031.141-4.235c0 0 1.339-.427 4.4 1.641a15.4 15.4 0 0 1 4-.541c1.36.009 2.719.187 4 .541c3.043-2.068 4.381-1.641 4.381-1.641c.859 2.204.317 3.833.161 4.235c1.015 1.12 1.635 2.547 1.635 4.297c0 6.145-3.74 7.5-7.296 7.891c.556.479 1.077 1.464 1.077 2.959c0 2.14-.02 3.864-.02 4.385c0 .416.28.916 1.104.755c6.4-2.093 10.979-8.093 10.979-15.156c0-8.833-7.161-16-16-16z"
-      ></path>
+      />
     </svg>
   )
 }
 
 export function HomeIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+    <svg role="img" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <title>ホームアイコン</title>
       <g
         fill="none"
         stroke="currentColor"
@@ -21,8 +23,8 @@ export function HomeIcon(props: SVGProps<SVGSVGElement>) {
         strokeLinejoin="round"
         strokeWidth={1.5}
       >
-        <path d="M6.133 21C4.955 21 4 20.02 4 18.81v-8.802c0-.665.295-1.295.8-1.71l5.867-4.818a2.09 2.09 0 0 1 2.666 0l5.866 4.818c.506.415.801 1.045.801 1.71v8.802c0 1.21-.955 2.19-2.133 2.19z"></path>
-        <path d="M9.5 21v-5.5a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2V21"></path>
+        <path d="M6.133 21C4.955 21 4 20.02 4 18.81v-8.802c0-.665.295-1.295.8-1.71l5.867-4.818a2.09 2.09 0 0 1 2.666 0l5.866 4.818c.506.415.801 1.045.801 1.71v8.802c0 1.21-.955 2.19-2.133 2.19z" />
+        <path d="M9.5 21v-5.5a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2V21" />
       </g>
     </svg>
   )
@@ -30,16 +32,11 @@ export function HomeIcon(props: SVGProps<SVGSVGElement>) {
 
 export function ProjectIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      viewBox="0 0 256 256"
-      {...props}
-    >
+    <svg role="img" width="1em" height="1em" viewBox="0 0 256 256" {...props}>
+      <title>Projectアイコン</title>
       <g fill="currentColor">
-        <path d="m240 128l-48 40H64l-48-40l48-40h128Z" opacity={0.2}></path>
-        <path d="M69.12 94.15L28.5 128l40.62 33.85a8 8 0 1 1-10.24 12.29l-48-40a8 8 0 0 1 0-12.29l48-40a8 8 0 0 1 10.24 12.3m176 27.7l-48-40a8 8 0 1 0-10.24 12.3L227.5 128l-40.62 33.85a8 8 0 1 0 10.24 12.29l48-40a8 8 0 0 0 0-12.29m-82.39-89.37a8 8 0 0 0-10.25 4.79l-64 176a8 8 0 0 0 4.79 10.26A8.1 8.1 0 0 0 96 224a8 8 0 0 0 7.52-5.27l64-176a8 8 0 0 0-4.79-10.25"></path>
+        <path d="m240 128l-48 40H64l-48-40l48-40h128Z" opacity={0.2} />
+        <path d="M69.12 94.15L28.5 128l40.62 33.85a8 8 0 1 1-10.24 12.29l-48-40a8 8 0 0 1 0-12.29l48-40a8 8 0 0 1 10.24 12.3m176 27.7l-48-40a8 8 0 1 0-10.24 12.3L227.5 128l-40.62 33.85a8 8 0 1 0 10.24 12.29l48-40a8 8 0 0 0 0-12.29m-82.39-89.37a8 8 0 0 0-10.25 4.79l-64 176a8 8 0 0 0 4.79 10.26A8.1 8.1 0 0 0 96 224a8 8 0 0 0 7.52-5.27l64-176a8 8 0 0 0-4.79-10.25" />
       </g>
     </svg>
   )
@@ -47,18 +44,20 @@ export function ProjectIcon(props: SVGProps<SVGSVGElement>) {
 
 export function StacksIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+    <svg role="img" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <title>Stacksアイコン</title>
       <path
         fill="currentColor"
         d="M12 13.187q-.192 0-.372-.053t-.367-.14L3.39 8.914q-.218-.131-.32-.318q-.103-.186-.103-.404t.102-.403q.103-.187.32-.318l7.874-4.08q.186-.087.366-.14T12 3.198t.372.053t.367.14l7.892 4.08q.217.131.32.318q.103.186.103.403t-.103.404t-.32.318l-7.892 4.08q-.187.087-.367.14t-.372.053m0 2.865l8.158-4.198q.069-.045.34-.01q.175.039.27.163t.096.299q0 .14-.058.25t-.198.182l-7.87 4.064q-.186.087-.366.14q-.18.052-.372.052t-.372-.053q-.18-.052-.366-.139l-7.85-4.063q-.14-.073-.208-.183t-.067-.25q0-.175.104-.3q.105-.123.28-.162q.087-.038.17-.04q.084 0 .17.05zm0 3.808l8.158-4.198q.069-.045.34-.01q.175.039.27.163t.096.299q0 .14-.058.25t-.198.182l-7.87 4.064q-.186.086-.366.139t-.372.053t-.372-.053t-.366-.14l-7.85-4.063q-.14-.073-.208-.182q-.067-.11-.067-.25q0-.175.104-.3q.105-.123.28-.162q.087-.038.17-.04q.084 0 .17.05z"
-      ></path>
+      />
     </svg>
   )
 }
 
 export function NoteIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <title>Noteアイコン</title>
       <g
         fill="none"
         stroke="currentColor"
@@ -67,8 +66,8 @@ export function NoteIcon(props: SVGProps<SVGSVGElement>) {
         strokeWidth={1.5}
         color="currentColor"
       >
-        <path d="m12.88 7.017l4.774 1.271m-5.796 2.525l2.386.636m-2.267 6.517l.954.255c2.7.72 4.05 1.079 5.114.468c1.063-.61 1.425-1.953 2.148-4.637l1.023-3.797c.724-2.685 1.085-4.027.471-5.085s-1.963-1.417-4.664-2.136l-.954-.255c-2.7-.72-4.05-1.079-5.113-.468c-1.064.61-1.426 1.953-2.15 4.637l-1.022 3.797c-.724 2.685-1.086 4.027-.471 5.085c.614 1.057 1.964 1.417 4.664 2.136"></path>
-        <path d="m12 20.946l-.952.26c-2.694.733-4.04 1.1-5.102.477c-1.06-.622-1.422-1.99-2.143-4.728l-1.021-3.872c-.722-2.737-1.083-4.106-.47-5.184C2.842 6.966 4 7 5.5 7"></path>
+        <path d="m12.88 7.017l4.774 1.271m-5.796 2.525l2.386.636m-2.267 6.517l.954.255c2.7.72 4.05 1.079 5.114.468c1.063-.61 1.425-1.953 2.148-4.637l1.023-3.797c.724-2.685 1.085-4.027.471-5.085s-1.963-1.417-4.664-2.136l-.954-.255c-2.7-.72-4.05-1.079-5.113-.468c-1.064.61-1.426 1.953-2.15 4.637l-1.022 3.797c-.724 2.685-1.086 4.027-.471 5.085c.614 1.057 1.964 1.417 4.664 2.136" />
+        <path d="m12 20.946l-.952.26c-2.694.733-4.04 1.1-5.102.477c-1.06-.622-1.422-1.99-2.143-4.728l-1.021-3.872c-.722-2.737-1.083-4.106-.47-5.184C2.842 6.966 4 7 5.5 7" />
       </g>
     </svg>
   )
@@ -76,18 +75,20 @@ export function NoteIcon(props: SVGProps<SVGSVGElement>) {
 
 export function ZennIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <title>Zennアイコン</title>
       <path
         fill="currentColor"
         d="M.264 23.771h4.984a.8.8 0 0 0 .645-.352L19.614.874c.176-.293-.029-.645-.381-.645h-4.72a.63.63 0 0 0-.557.323L.03 23.361c-.088.176.029.41.234.41m17.181-.352l6.479-10.408a.477.477 0 0 0-.41-.733h-4.691a.52.52 0 0 0-.44.235l-6.655 10.643c-.176.264.029.616.352.616h4.779a.65.65 0 0 0 .586-.353"
-      ></path>
+      />
     </svg>
   )
 }
 
 export function LinkOutIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32" {...props}>
+    <svg role="img" width="1em" height="1em" viewBox="0 0 32 32" {...props}>
+      <title>外部リンクアイコン</title>
       <path
         stroke="currentColor"
         strokeWidth="2"
@@ -96,7 +97,7 @@ export function LinkOutIcon(props: SVGProps<SVGSVGElement>) {
         height="1em"
         viewBox="0 0 32 32"
         d="M22 3h7v7m-1.5-5.5L20 12m-3-7H8a3 3 0 0 0-3 3v16a3 3 0 0 0 3 3h16a3 3 0 0 0 3-3v-9"
-      ></path>
+      />
     </svg>
   )
 }
@@ -104,7 +105,7 @@ export function LinkOutIcon(props: SVGProps<SVGSVGElement>) {
 export function MonitorIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
+      role="img"
       width="24"
       height="24"
       viewBox="0 0 24 24"
@@ -115,6 +116,7 @@ export function MonitorIcon(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
+      <title>Montorアイコン</title>
       <rect width="20" height="14" x="2" y="3" rx="2" />
       <line x1="8" x2="16" y1="21" y2="21" />
       <line x1="12" x2="12" y1="17" y2="21" />
@@ -125,7 +127,7 @@ export function MonitorIcon(props: SVGProps<SVGSVGElement>) {
 export function SunIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
+      role="img"
       width="24"
       height="24"
       viewBox="0 0 24 24"
@@ -136,6 +138,7 @@ export function SunIcon(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
+      <title>ライトモード</title>
       <circle cx="12" cy="12" r="4" />
       <path d="M12 2v2" />
       <path d="M12 20v2" />
@@ -152,7 +155,7 @@ export function SunIcon(props: SVGProps<SVGSVGElement>) {
 export function MoonIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
+      role="img"
       width="24"
       height="24"
       viewBox="0 0 24 24"
@@ -163,6 +166,7 @@ export function MoonIcon(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
+      <title>ダークモード</title>
       <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
     </svg>
   )
@@ -170,17 +174,8 @@ export function MoonIcon(props: SVGProps<SVGSVGElement>) {
 
 export function TagIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      {...props}
-    >
+    <svg role="img" width="24" height="24" viewBox="0 0 24 24" {...props}>
+      <title>タグアイコン</title>
       <path d="M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8.704 8.704a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42z" />
       <circle cx="7.5" cy="7.5" r=".5" fill="currentColor" />
     </svg>
