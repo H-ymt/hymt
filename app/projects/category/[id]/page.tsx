ce4c50ce7fa7bb532bd72e7b2fd390714e2e271f
半代大和 <y.handai1272@gmail.com>
2025-03-30 18:54:16 +0900

    feat: Add category list and update category-related pages

--- app/projects/category/[id]/page.tsx
+++ app/projects/category/[id]/page.tsx
@@ -1,6 +1,10 @@
+import Link from 'next/link'
+
 import Container from '@/app/components/container'
 import ProjectList from '@/app/components/project-list'
-import { getCategoryDetail, getProjectsList } from '@/lib/microcms'
+import { getCategoryDetail, getCategoryList, getProjectsList } from '@/lib/microcms'
+
+import styles from './page.module.css'
 
 type Props = {
   params: Promise<{
@@ -13,13 +17,23 @@ export default async function Page(props: Props) {
   const { contents: project } = await getProjectsList({
     filters: `category[contains]${params.id}`,
   })
-
+  const categories = await getCategoryList()
   const category = await getCategoryDetail(params.id)
 
   return (
     <Container type="subpage">
-      <p>{category.name}</p>
-      <ProjectList projects={project} />
+      <p>Tag: {category.name}</p>
+      <ul className={styles.category}>
+        {categories &&
+          categories.contents.map((category) => (
+            <li key={category.id} className={styles.categoryItem}>
+              <Link href={`/projects/category/${category.id}`}>{category.name}</Link>
+            </li>
+          ))}
+      </ul>
+      <div className={styles.projects}>
+        <ProjectList projects={project} />
+      </div>
     </Container>
   )
 }
