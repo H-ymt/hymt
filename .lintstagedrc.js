9c54481d7e716ae8b583ea8157f69c4604a6c2c0
H-ymt <y.handai1272@gmail.com>
2025-08-31 20:59:19 +0900

    feat(*): リポジトリ初期化

--- .lintstagedrc.js
+++
@@ -1,8 +0,0 @@
-const { relative } = require('node:path')
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => relative(process.cwd(), f)).join(' --file ')}`
-
-module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write'],
-}
